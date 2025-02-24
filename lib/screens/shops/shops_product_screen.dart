119c166cdbac760a1c147057a774132d28ceecd5
Wahab's  Mac <pak@MacBook-Pro.local>
2025-02-25 02:54:14 +0500

    wahab

--- lib/screens/shops/shops_product_screen.dart
+++ lib/screens/shops/shops_product_screen.dart
@@ -1,5 +1,5 @@
-import 'package:chotu_admin/screens/shops/shops_product_screen.dart';
 import 'package:chotu_admin/screens/shops/widgets/addNewShopDialogBox.dart';
+import 'package:chotu_admin/screens/shops/widgets/addShopProductDialogBox.dart';
 import 'package:chotu_admin/utils/app_Colors.dart';
 import 'package:chotu_admin/utils/app_Paddings.dart';
 import 'package:chotu_admin/utils/app_text_widgets.dart';
@@ -11,12 +11,12 @@ import 'package:intl/intl.dart';
 
 import '../../generated/assets.dart';
 
-  class ShopsScreen extends StatefulWidget {
+class ShopProductsScreen extends StatefulWidget {
   @override
-  State<ShopsScreen> createState() => _ShopsScreenState();
+  State<ShopProductsScreen> createState() => _ShopProductsScreenState();
 }
 
-class _ShopsScreenState extends State<ShopsScreen> {
+class _ShopProductsScreenState extends State<ShopProductsScreen> {
   TextEditingController startDateController = TextEditingController();
   TextEditingController endDateController = TextEditingController();
 
@@ -43,11 +43,21 @@ class _ShopsScreenState extends State<ShopsScreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
 
+                  /// back icon
+                  InkWell(
+                    overlayColor: WidgetStatePropertyAll<Color>(Colors.transparent),
+                      onTap: (){
+                        Navigator.pop(context);
+                      },
+                      child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Icon(Icons.arrow_back_ios))),
+                  padding30,
                   /// Status Summary Cards
                   Row(
                     children: [
                       Expanded(
-                        child: _buildStatsCard('Total Shops', 5, Colors.orange),
+                        child: _buildStatsCard('Total Products', 5, Colors.orange),
                       ),
                       padding12,
                       Expanded(
@@ -74,7 +84,7 @@ class _ShopsScreenState extends State<ShopsScreen> {
                           obscureText: false,
                           textInputAction: TextInputAction.search,
                           keyboardType: TextInputType.text,
-                          hintText: 'Search Shops by name',
+                          hintText: 'Search Product by name',
                           suffixIcon: SizedBox(
                             height: 24,
                             width: 24,
@@ -87,7 +97,7 @@ class _ShopsScreenState extends State<ShopsScreen> {
                       padding12,
                       InkWell(
                         onTap: () {
-                          showAddShopDialog(context);
+                          showAddShopProductDialog(context);
                         },
                         child: Align(
                           alignment: Alignment.topRight,
@@ -98,7 +108,7 @@ class _ShopsScreenState extends State<ShopsScreen> {
                               color: AppColors.primaryColor,
                             ),
                             child: Text(
-                              'Add New Shop',
+                              'Add New Product',
                               style: getSemiBoldStyle(
                                 color: AppColors.whiteColor,
                                 fontSize: 16,
@@ -122,7 +132,7 @@ class _ShopsScreenState extends State<ShopsScreen> {
                     spacing: 16,
                     runSpacing: 16,
                     children: List.generate(10, (index) {
-                      return _buildShopCard(context);
+                      return _buildProductCard(context);
                     }),
                   ),
                 ],
@@ -133,7 +143,7 @@ class _ShopsScreenState extends State<ShopsScreen> {
       ),
     );
   }
-/// build stats card
+  /// build stats card
   Widget _buildStatsCard(String title, int count, Color color) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
@@ -160,85 +170,142 @@ class _ShopsScreenState extends State<ShopsScreen> {
       ),
     );
   }
+/// build product Card
+  Widget _buildProductCard(BuildContext context) {
+    bool isActive = true; // Initial status (you can fetch this from your backend)
 
-  /// build shop card
-  Widget _buildShopCard(BuildContext context) {
     return FractionallySizedBox(
       widthFactor: 1 / 3.16, // Takes 1/3 of the parent width
-      child: InkWell(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (_){
-            return
-              ShopProductsScreen()
-              ;
-          }));
-        },
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(color: Colors.white, boxShadow: [
-            BoxShadow(
-                offset: const Offset(0, 0),
-                blurRadius: 7,
-                spreadRadius: 0,
-                color: Colors.black.withOpacity(.25)),
-          ]),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 51,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.topRight,
-                    colors: [
-                      Color(0xff45CF8D),
-                      Color(0xff046938),
-                    ],
-                  ),
-                ),
-                child: Center(
-                    child: Text(
-                      'Shop Name - xxxxx',
-                      style: getMediumStyle(color: Colors.white, fontSize: 16),
-                    )),
-              ),
-              Padding(
-                padding:
-                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 14),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'Shop Details',
-                          style: getMediumStyle(
-                              color: AppColors.textColor, fontSize: 16),
-                        ),
-                        Spacer(),
-                        SizedBox()
-                      ],
-                    ),
-                    padding16,
-                    _buildRow(title: 'Category', description: 'Mobile Shop'),
-                    _buildRow(title: 'Total Products', description: '7'),
-                    _buildRow(title: 'Total Orders ', description: '10'),
-                    _buildRow(title: 'Delivered Orders', description: '5'),
-                    _buildRow(title: 'Shop Location', description: 'Johr Town Lahore'),
-                    padding16,
-                    const Divider(
-                      color: Color(0xffAFA9A9),
-                    ),
-                    _buildRow(title: 'Shop Status', description: 'Active'),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              offset: const Offset(0, 0),
+              blurRadius: 7,
+              spreadRadius: 0,
+              color: Colors.black.withOpacity(.25)),
+        ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+
+            /// Product Name
+            Container(
+              height: 51,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    Color(0xff45CF8D),
+                    Color(0xff046938),
                   ],
                 ),
               ),
-            ],
-          ),
+              child: Center(
+                child: Text(
+                  'Product Name - iPhone 11 Pro Max',
+                  style: getMediumStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+            /// Product Image
+            Container(
+              height: 150,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2uHNWvGuM8JNluffrpa-t7oBVbmd3YqFIlA&s'), // Replace with actual image URL
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+
+            Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 10.0, vertical: 14),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Product Details',
+                        style: getMediumStyle(
+                            color: AppColors.textColor, fontSize: 16),
+                      ),
+                      Spacer(),
+                      SizedBox()
+                    ],
+                  ),
+                  padding16,
+                  _buildRow(title: 'Category', description: 'Mobile Shop'),
+                  _buildRow(title: 'Product Price', description: 'Best Selling Products'),
+                  _buildRow(title: 'Product Detail', description: '10'),
+                  _buildRow(
+                      title: 'Shop Name', description: 'Johr Town Lahore Branch'),
+                  padding16,
+                  const Divider(
+                    color: Color(0xffAFA9A9),
+                  ),
+
+                  // Product Status Switch
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Product Status',
+                        style: getMediumStyle(
+                            color: AppColors.textColor, fontSize: 16),
+                      ),
+                      Switch(
+                        value: isActive,
+                        activeColor: Colors.green,
+                        onChanged: (value) {
+                          _showConfirmationDialog(context, value);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+/// Function to show confirmation dialog
+  void _showConfirmationDialog(BuildContext context, bool newValue) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Confirm Action"),
+          content: Text(newValue
+              ? "Are you sure you want to activate this product?"
+              : "Are you sure you want to deactivate this product?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Here you can update the product status in your backend
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text("Yes"),
+            ),
+          ],
+        );
+      },
     );
   }
 
